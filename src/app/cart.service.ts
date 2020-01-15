ddd8f1436436032f1ecfc85f71ec9dc2a5cfe76b
binduputta <binduhimaputta@gmail.com>
2020-01-15 13:15:37 -0600

    3 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
-
-@Injectable()
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+   providedIn: 'root'
+}
+)
 export class CartService {
+  items =[];
 
-  constructor() { }
 
+  constructor(
+    private http: HttpClient
+  ) {}
+
+addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
 }
